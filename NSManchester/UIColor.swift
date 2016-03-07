835bd4955cb0fb45227b50cb4ed2b653934020a6
tailec <pawel@tailec.com>
2016-03-07 20:23:02 +0000

    Add UIColor extensions

---
+++ NSManchester/UIColor.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIColor {
+    static func burntSiennaColor() -> UIColor {
+        return UIColor(red: 238.0/255.0, green: 121.0/255.0, blue: 101.0/255.0, alpha: 1.0)
+    }
+    
+    static func hopbushColor() -> UIColor {
+        return UIColor(red: 192.0/255.0, green: 105.0/255.0, blue: 155.0/255.0, alpha: 1.0)
+    }
+    
+    static func waxFlowerColor() -> UIColor {
+        return UIColor(red: 239.0/255.0, green: 173.0/255.0, blue: 150.0/255.0, alpha: 1.0)
+    }
+}
